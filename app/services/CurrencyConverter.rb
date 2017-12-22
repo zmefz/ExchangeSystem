b865c41f190e1aafc650a969ade02c264cf45e5b
zmefz <saigakj@inbox.ru>
2017-12-22 09:29:04 +0300

    change currencies calculating

--- app/services/CurrencyConverter.rb
+++ app/services/CurrencyConverter.rb
@@ -5,10 +5,23 @@ class CurrencyConverter
     units_to_currency(currency_to, units)
   end
 
+  def self.convert_reverse(currency_from, currency_to, amount = 1)
+    units = currency_to_units_reverse(currency_to, amount)
+    units_to_currency_reverse(currency_from, units)
+  end
+
   private
+
+  def self.currency_to_units_reverse(currency, amount)
+    (amount * currency.sell_value).ceil(2)
+  end
 
   def self.currency_to_units(currency, amount)
     (amount * currency.buy_value).ceil(2)
+  end
+
+  def self.units_to_currency_reverse(currency, amount)
+    (amount / currency.buy_value).floor(2)
   end
 
   def self.units_to_currency(currency, amount)
--- app/services/RelativeCurrenciesCalculator.rb
+++ app/services/RelativeCurrenciesCalculator.rb
@@ -4,9 +4,17 @@ class RelativeCurrenciesCalculator
     result = []
 
     Currency.active.find_each do |currency|
+
+      calculated_amount = nil
+      if default_currency.id == currency.id
+        calculated_amount = 1
+      else
+        calculated_amount = CurrencyConverter.convert_reverse(default_currency, currency, amount)
+      end
+
       result.append({
         currency: currency,
-        amount:   CurrencyConverter.convert(default_currency, currency, amount)
+        amount:   calculated_amount,
       }) if !default_currency_set || (default_currency_set && !(currency.id == default_currency.id))
     end
 
